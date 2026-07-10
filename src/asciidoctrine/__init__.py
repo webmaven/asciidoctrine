8118c929f2b254a83fd9cae6e0d8aa8dadd9321e
Michael R. Bernstein <zopemaven@gmail.com>
2026-07-09 19:48:44 -0600

    release: v0.1.0a2

--- src/asciidoctrine/__init__.py
+++ src/asciidoctrine/__init__.py
@@ -5,7 +5,7 @@ Asciidoctrine: Pure-Python AsciiDoc parser library using Lark.
 from .lark_parser import parse_to_ast
 from .nodes import Document, Node, NodeVisitor, Paragraph, Section, Text
 
-__version__ = "0.1.0"
+__version__ = "0.1.0a2"
 
 __all__ = [
     "parse_to_ast",
